c6f217faff98e6d47f6c2467b0a5cf9fc74ca789
berg <berg@tu-berlin.de>
2020-12-13 00:41:10 +0100

    extract_vb_samples function

--- tests/test_vb_extract.py
+++ tests/test_vb_extract.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 import vb_extract
@@ -22,3 +23,64 @@ class Test_parse_bracketed_param_name:
         """Test raise ValueError for invalid param_names"""
         with pytest.raises(ValueError):
             vb_extract.parse_bracketed_param_name(param_name)
+
+
+class Test_extract_vb_samples:
+
+    @pytest.mark.parametrize("stan_fit,expected", [
+        ({
+            "sampler_param_names": [],
+            "sampler_params": []
+        }, {}),
+        ({
+            "sampler_param_names": ["alpha"],
+            "sampler_params": [[0]]
+        }, {
+            "alpha": np.array([0])
+        }),
+        ({
+            "sampler_param_names": ["a[2]", "a[1]"],
+            "sampler_params": [[1, 3, 5], [0, 2, 4]]
+        }, {
+            "a": np.arange(6).reshape((3, 2))
+        }),
+        ({
+            "sampler_param_names": ["a[2,1]", "b", "a[1,1]"],
+            "sampler_params": [[1, 3, 5], [6, 7, 8], [0, 2, 4]]
+        }, {
+            "a": np.arange(6).reshape((3, 2, 1)),
+            "b": np.array([6, 7, 8])
+        }),
+        ({
+            "sampler_param_names": ["a[2,1]", "b", "a[1,1]"],
+            "sampler_params": [[], [], []]
+        }, {
+            "a": np.empty((0, 2, 1)),
+            "b": np.empty((0,)),
+        }),
+    ])
+    def test_extract_vb_samples(self, stan_fit, expected):
+        np.testing.assert_equal(expected,
+                                vb_extract.extract_vb_samples(stan_fit))
+
+    @pytest.mark.parametrize("stan_fit", [
+        {
+            "sampler_param_names": ["a", "b"],
+            "sampler_params": [[0], [0, 1]],
+        },
+        {
+            "sampler_param_names": ["a[2,1]", "b", "a[1,1]"],
+            "sampler_params": [[1], [6, 8], [2, 4]]
+        },
+        {
+            "sampler_param_names": ["a[2,1]", "b", "a[1,1]"],
+            "sampler_params": [[1, 2], [6, 8], [2, 4, 7]]
+        },
+        {
+            "sampler_param_names": ["a[2,1]", "b", "a[1,1]"],
+            "sampler_params": [[1, 3, 5], [6, 8], [0, 2, 4]]
+        },
+    ])
+    def test_raises_value_error_invalid_sample_num(self, stan_fit):
+        with pytest.raises(ValueError):
+            vb_extract.extract_vb_samples(stan_fit)
--- vb_extract.py
+++ vb_extract.py
@@ -36,3 +36,36 @@ def parse_bracketed_param_name(param_name: str) -> Tuple[str, Tuple[int, ...]]:
     indices = tuple() if match.group("indices") is None else match.group(
         "indices").split(",")
     return name, tuple(int(x) for x in indices)
+
+
+def extract_vb_samples(stan_vb_results: collections.OrderedDict) -> Dict:
+    """Extract samples from a stan fit object true to size.
+
+    Args:
+        stan_vb_results: as returned from pystan.StanModel.vb(...)
+
+    Returns:
+        dict: contains samples in combined by size
+    """
+    num_samples = len(stan_vb_results["sampler_params"]
+                      [0]) if stan_vb_results["sampler_params"] else 0
+    parameter_shapes = collections.defaultdict(list)
+    parameter_indices = {}
+    for param_index, raw_name in enumerate(
+            stan_vb_results["sampler_param_names"]):
+        name, indices = parse_bracketed_param_name(raw_name)
+        shape = [int(x) for x in indices]
+        indices = tuple(int(x) - 1 for x in indices)
+        parameter_shapes[name].append(shape)
+        parameter_indices[param_index] = (name, indices)
+
+    samples = {
+        name: np.empty((num_samples,) + tuple(np.array(shape).max(axis=0)))
+        for name, shape in parameter_shapes.items()
+    }
+
+    for param_index, (name, indices) in parameter_indices.items():
+        samples[name][(...,) +
+                      indices] = stan_vb_results["sampler_params"][param_index]
+
+    return samples
